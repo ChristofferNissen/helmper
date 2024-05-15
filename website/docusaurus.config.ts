f7a40600d0b1171d19284fb0f19f246233fdf3ca
Christoffer Nissen <christoffer.nissen@gmail.com>
2024-05-16 00:54:21 +0200

    changed basepath

--- website/docusaurus.config.ts
+++ website/docusaurus.config.ts
@@ -18,7 +18,7 @@ const config: Config = {
   url: 'https://christoffernissen.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/helmper/website/',
+  baseUrl: '/helmper/',
 
   trailingSlash: false,
 
