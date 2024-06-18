1910cb737f031ecc25dee58f65b113ab6a4cb9c8
Christoffer Nissen <christoffer.nissen@gmail.com>
2024-06-18 09:11:02 +0200

    removed glob resolving

--- pkg/helm/chartImportOption.go
+++ pkg/helm/chartImportOption.go
@@ -32,12 +32,6 @@ func (opt ChartImportOption) Run(ctx context.Context, setters ...Option) error {
 
 	charts := []Chart{}
 	for _, c := range opt.ChartCollection.Charts {
-		// Resolve Globs to latest patch
-		v, err := c.ResolveVersion()
-		if err != nil {
-			return err
-		}
-		c.Version = v
 		charts = append(charts, c)
 
 		_, chartRef, values, err := c.Read(args.Update)
