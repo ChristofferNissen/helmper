118139375a271323e5733abefd8ae8f7771a9e24
Christoffer Nissen <christoffer.nissen@gmail.com>
2024-10-16 11:57:04 +0200

    [fix] trivy ignore unfixed homebrewed solution by using trivy lib filter function (#117)

--- pkg/trivy/util.go
+++ pkg/trivy/util.go
@@ -7,7 +7,7 @@ import (
 func ContainsOsPkgs(rs types.Results) bool {
 	for _, r := range rs {
 		switch r.Class {
-		case "os-pkgs":
+		case types.ClassOSPkg:
 			if !r.IsEmpty() {
 				return true
 			}
